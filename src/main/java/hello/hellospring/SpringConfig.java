0a27f8cc3947cbd89f9a258709ec7ba4a9ff2325
Nuri Lee <leenuri67@gmail.com>
2023-11-07 18:11:57 +0900

    spring Jdbc Template

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,6 +1,7 @@
 package hello.hellospring;
 
 import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
@@ -32,7 +33,8 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository() {
 //       return new MemoryMemberRepository();
-       return new JdbcMemberRepository(dataSource);
+//       return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
     // MemberService, MemberRepository를 스프링빈에 등록하고
 }
