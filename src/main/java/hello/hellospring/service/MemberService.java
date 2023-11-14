40f2e102f384900a6a464a272aa07fb97743082e
Nuri Lee <leenuri67@gmail.com>
2023-11-14 18:16:47 +0900

    되

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -61,17 +61,9 @@ public class MemberService {
         ->메서드로 Extract Method
         */
 
-        long start = System.currentTimeMillis();
-
-        try {
-            validateDuplicateMember(member);    //중복 회원 검증
-            memberRepository.save(member);
-            return member.getId();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("join = " + timeMs + "ms");
-        }
+        validateDuplicateMember(member);    //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
@@ -85,29 +77,8 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers() {
-        long start = System.currentTimeMillis();
-
-        try {
-            return memberRepository.findAll();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("findMembers " + timeMs + "ms");
-        }
+        return memberRepository.findAll();
     }
-
-    /**
-     * AOP가 필요한 상황
-     * 모든 메소드의 호출 시간을 측정하고 싶다면?
-     * 공통 관심 사항(cross-cutting concern) vs 핵심 관심 사항(core concern)
-     * 회원 가입 시간, 회원 조회 시간을 측정하고 싶다면?
-
-     * 문제점
-     시간을 측정하는 로직과 핵심 비즈니스 로직이 섞여서 유지보수가 어렵다.
-     시간을 측정하는 로직을 별도의 공통 로직으로 만들기 매우 어렵다.
-     시간을 측정하는 로직을 변경할 때 모든 로직을 찾아가면서 변경해야 한다.
-
-     */
 
     public Optional<Member> findOne(Long memberId) {
         return memberRepository.findById(memberId);
